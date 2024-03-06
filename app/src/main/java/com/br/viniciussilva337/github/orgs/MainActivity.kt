d3e5f2083941bf9f9c74cdecc8b823cb9475509b
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-06 20:46:44 -0300

    Criando layout

---
+++ app/src/main/java/com/br/viniciussilva337/github/orgs/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.br.viniciussilva337.github.orgs
+
+import android.app.Activity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.TextView
+
+class MainActivity : Activity () {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
